d39b429df285d0e7a0e6416f2e20b7589c390376
BKD471 <bhaskarkumardas9@gmail.com>
2023-03-28 02:55:11 +0530

    added a scratch ui

---
+++ jpmorgan-ui-interface/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Headre";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
