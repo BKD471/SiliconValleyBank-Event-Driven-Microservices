8fc526dd2ce49cc04a502f6ad6ed11c21b81ef14
BKD471 <bhaskarkumardas9@gmail.com>
2023-06-19 23:07:24 +0530

    adding log aspect

--- accounts-services/src/main/java/com/example/accountsservices/aspect/LoggerAspect.java
+++ accounts-services/src/main/java/com/example/accountsservices/aspect/LoggerAspect.java
@@ -1,8 +1,14 @@
 package com.example.accountsservices.aspect;
 
+import com.example.accountsservices.dto.outputDtos.OutputDto;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
 
 
 @Aspect
@@ -10,5 +16,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggerAspect {
 
-
+    @Around(value = "execution(com.example.accountsservices.dto.outputDtos.OutputDto  com.example.accountsservices.service.*.*(..))")
+    public OutputDto log(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("<---------------------------------->"+joinPoint.
+                getSignature().
+                toString() + " method executions starts---------------------------------------------------------------->");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsedInMs = Duration.between(start, end).toMillis();
+        log.info(String.format("<-----------------Time elapsed to execute %s in Ms is %s------------------------------------->", joinPoint.getSignature().toString(), timeElapsedInMs));
+        log.info("<--------------------------------->"+joinPoint.getSignature().toString() + "method execution stops------------------------------------>");
+        return  (OutputDto) result;
+    }
 }
