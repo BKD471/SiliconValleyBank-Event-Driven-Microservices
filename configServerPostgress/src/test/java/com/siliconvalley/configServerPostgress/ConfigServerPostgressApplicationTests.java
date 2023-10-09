2bee9189075b73b18a57a97a01fb8d3af8938b6b
BKD471 <bhaskarkumardas9@gmail.com>
2023-10-10 02:23:16 +0530

    added config servers

---
+++ configServerPostgress/src/test/java/com/siliconvalley/configServerPostgress/ConfigServerPostgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siliconvalley.configServerPostgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerPostgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
